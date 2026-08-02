874c121961cbcf504c04bdf68fa72c7a1a795834
alexanderortmann-rgb <alexander.ortmann@gmail.com>
2026-08-02 23:59:42 +0200

    Update fluobilder.js

--- fluobilder.js
+++ fluobilder.js
@@ -1,20 +1,18 @@
+function initFluoModalEvents() {
+
+    // Liste aller Buttons + zugehöriger Modals
+    const modalPairs = [
+        ['btHelpSt', 'modHelpSt'],
+        ['btHelpPl', 'modHelpPl'],
+        ['btHelpFl', 'modHelpFl']
+    ];
+
+    // Öffnen-Events setzen
+    modalPairs.forEach(([btnId, modalId]) => {
+        const btn = document.getElementById(btnId);
+        if (btn) {
+            btn.onclick = () => openModal(modalId);
+        }
+    });
 
 
-        // Event-Listener für die ?-Button zum Öffnen
-        document.getElementById('btHelpSt').onclick = function() {
-            openModal('modHelpSt');
-        };
-        document.getElementById('btHelpPl').onclick = function() {
-            openModal('modHelpPl');
-        };
-        document.getElementById('btHelpFl').onclick = function() {
-            openModal('modHelpFl');
-        };
-
-      
-        
-        var Mueller = "Quelle: Müller-Treiber, A. (2017) Kontaktlinsen Know-how. 4. Aufl. Heidelberg: DOZ. ISBN 978-3-942873-17-8";
-        // document.getElementById('Müller').textContent = Mueller;
-        document.querySelectorAll('.Mueller').forEach(element => {
-         element.textContent = Mueller;
-     });
