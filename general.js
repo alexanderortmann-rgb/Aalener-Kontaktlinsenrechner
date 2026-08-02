7d2514d633f17220a9c24f5ae45319c97654c915
alexanderortmann-rgb <alexander.ortmann@gmail.com>
2026-08-02 23:23:43 +0200

    Update general.js

--- general.js
+++ general.js
@@ -27,6 +27,26 @@ window.onload = function() {
             });
         }
     };
+
+    document.getElementById('a1').addEventListener('input', function() {
+    	var a1 = parseFloat(document.getElementById('a1').value);
+    	if (a1 < 0) {
+    	    a1 += 180;
+    	} else if (a1 >= 180) {
+    	    a1 -= 180;
+    	}
+    	document.getElementById('a1').value = a1;
+    });
+    
+    document.getElementById('a2').addEventListener('input', function() {
+    	var a1 = parseFloat(document.getElementById('a2').value);
+    	if (a1 < 0) {
+    	    a1 += 180;
+    	} else if (a1 >= 180) {
+    	    a1 -= 180;
+    	}
+    	document.getElementById('a2').value = a1;
+    });
         
 
 /*
