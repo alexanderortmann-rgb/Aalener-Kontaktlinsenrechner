2b1975c3644b85491f18d5c8a494ad9d19e90b51
alexanderortmann-rgb <alexander.ortmann@gmail.com>
2026-08-02 23:55:51 +0200

    Update materialien.js

--- materialien.js
+++ materialien.js
@@ -1,21 +1,17 @@
+function initMaterialienModalEvents() {
+    const ids = [
+        ['btHelpPermeabilität', 'modHelpPermeabilität'],
+        ['btHelpBenetzungswinkel', 'modHelpBenetzungswinkel'],
+        ['btHelpDynBen', 'modHelpDynBen'],
+        ['btHelpHärte', 'modHelpHärte'],
+        ['btHelpModulus', 'modHelpModulus'],
+        ['btHelpZaehi', 'modHelpZaehi']
+    ];
 
-
-// Event-Listener für die ?-Button zum Öffnen
-document.getElementById('btHelpPermeabilität').onclick = function() {
-    openModal('modHelpPermeabilität');
-};
-document.getElementById('btHelpBenetzungswinkel').onclick = function() {
-    openModal('modHelpBenetzungswinkel');
-};
-document.getElementById('btHelpDynBen').onclick = function() {
-    openModal('modHelpDynBen');
-};
-document.getElementById('btHelpHärte').onclick = function() {
-    openModal('modHelpHärte');
-};
-document.getElementById('btHelpModulus').onclick = function() {
-    openModal('modHelpModulus');
-};
-document.getElementById('btHelpZaehi').onclick = function() {
-    openModal('modHelpZaehi');
-};
+    ids.forEach(([btnId, modalId]) => {
+        const btn = document.getElementById(btnId);
+        if (btn) {
+            btn.onclick = () => openModal(modalId);
+        }
+    });
+}
