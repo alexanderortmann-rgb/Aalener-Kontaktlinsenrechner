bc233b9f8af09ecce4312742dbb5f56393d94a65
alexanderortmann-rgb <alexander.ortmann@gmail.com>
2026-08-02 23:33:47 +0200

    Update materialien.js

--- materialien.js
+++ materialien.js
@@ -1,14 +1,4 @@
-// Funktion zum Öffnen
-function openModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "block";
-}
 
-// Funktion zum Schließen
-function closeModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "none";
-}
 
 // Event-Listener für die ?-Button zum Öffnen
 document.getElementById('btHelpPermeabilität').onclick = function() {
@@ -29,19 +19,3 @@ document.getElementById('btHelpModulus').onclick = function() {
 document.getElementById('btHelpZaehi').onclick = function() {
     openModal('modHelpZaehi');
 };
-
-// Event-Listener für die X in den Modals
-var closeButtons = document.getElementsByClassName('close');
-for (var i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].onclick = function() {
-        var modalId = this.getAttribute('data-modal');
-        closeModal(modalId);
-    };
-}
-
-// Schließen beim Klicken außerhalb des Fensters
-window.onclick = function(event) {
-    if (event.target.classList.contains('modal')) {
-        closeModal(event.target.id);
-    }
-}
