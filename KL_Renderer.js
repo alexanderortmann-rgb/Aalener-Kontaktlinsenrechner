757cc29be31ca27cfdca00f3b90026fd37366227
alexanderortmann-rgb <alexander.ortmann@gmail.com>
2026-08-03 01:02:50 +0200

    Update KL_Renderer.js

--- KL_Renderer.js
+++ KL_Renderer.js
@@ -1,1 +1,19 @@
+let scene, camera, renderer;
+
+function initKLVisualizer() {
+    const container = document.getElementById('kl3dContainer');
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(45, container.clientWidth / 400, 0.1, 1000);
+    camera.position.set(0, -40, 20);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(container.clientWidth, 400);
+    container.appendChild(renderer.domElement);
+
+    addCornea();
+    addLens();
+
+    animate();
+}
 
